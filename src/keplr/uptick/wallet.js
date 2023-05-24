d413e4f245f4d92a88c937cbd258633a39b9029c
TheCodeor <447690182@qq.com>
2023-05-24 19:31:30 +0800

    更改代理

--- src/keplr/uptick/wallet.js
+++ src/keplr/uptick/wallet.js
@@ -17,8 +17,8 @@ import {
 
 const chainId = "origin_1170-1";
 const irisChainId = "gon-irishub-1";
-const uptickUrl = "http://localhost:8080/uptick";
-const irisUrl = "http://localhost:8080/iris";
+const uptickUrl = window.location.protocol+"//"+ window.location.host + "/uptick";
+const irisUrl = window.location.protocol+"//"+ window.location.host + "/iris";
 
 
 
